71e81aff094cf86f959a469c7964a0a04c2e4540
ImtiazKabir <imtiazkabir.imtiaz@gmail.com>
2025-07-27 19:56:23 +0600

    Fix CI test failure by configuring H2 database for ArebbusApplicationTests

--- src/test/java/com/project/arebbus/ArebbusApplicationTests.java
+++ src/test/java/com/project/arebbus/ArebbusApplicationTests.java
@@ -2,10 +2,16 @@ package com.project.arebbus;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+    "spring.datasource.url=jdbc:h2:mem:testdb",
+    "spring.datasource.driver-class-name=org.h2.Driver",
+    "spring.jpa.hibernate.ddl-auto=create-drop"
+})
 class ArebbusApplicationTests {
-//
+
     @Test
     void contextLoads() {}
 
