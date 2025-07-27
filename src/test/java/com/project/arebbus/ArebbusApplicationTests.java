084e2da733ca3afaa5c74d2901eaeb6fa04f1efb
ImtiazKabir <imtiazkabir.imtiaz@gmail.com>
2025-07-27 20:20:34 +0600

    Fix test env

--- src/test/java/com/project/arebbus/ArebbusApplicationTests.java
+++ src/test/java/com/project/arebbus/ArebbusApplicationTests.java
@@ -8,7 +8,9 @@ import org.springframework.test.context.TestPropertySource;
 @TestPropertySource(properties = {
     "spring.datasource.url=jdbc:h2:mem:testdb",
     "spring.datasource.driver-class-name=org.h2.Driver",
-    "spring.jpa.hibernate.ddl-auto=create-drop"
+    "spring.jpa.hibernate.ddl-auto=create-drop",
+    "FRONTEND_URL=http://localhost:3000",
+    "SECRET_KEY=test-secret-key-for-testing-only"
 })
 class ArebbusApplicationTests {
 
